2f9a21783524e732174b54e3d298d89e4e266bde
Chris Fattarsi <1092859+fattarsi@users.noreply.github.com>
2026-02-25 21:31:49 -0700

    Update xrandr outputs for new laptop

--- bin/external-only.sh
+++ bin/external-only.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+xrandr --output eDP-1 --off --output HDMI-1-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-1-0 --off
