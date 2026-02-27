914929a1db022834459833f17544f42c3e482c4c
Chris Fattarsi <1092859+fattarsi@users.noreply.github.com>
2026-02-27 09:57:51 -0700

    Remove keyboard layout widget from top bar

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -372,7 +372,6 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
             wibox.widget.systray(),
             battery,
             mytextclock,
