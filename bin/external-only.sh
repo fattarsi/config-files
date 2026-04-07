bdf250943ff66c385186d310a179f404068c3be2
Chris Fattarsi <1092859+fattarsi@users.noreply.github.com>
2026-04-06 20:07:01 -0600

    chore: remove unused bin scripts

--- bin/external-only.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-HDMI_OUT=$(xrandr | grep -oP 'HDMI-\S+ connected' | head -1 | awk '{print $1}')
-EDP_OUT=$(xrandr | grep -oP 'eDP-\S+ connected' | head -1 | awk '{print $1}')
-xrandr --output "${EDP_OUT:-eDP-1}" --off --output "${HDMI_OUT:-HDMI-1-0}" --mode 3840x2160 --pos 0x0 --rotate normal
